4d2a9e40c19cc77b68d2faafd2e4900be855e16e
mccoy88f <50622699+mccoy88f@users.noreply.github.com>
2025-01-14 13:02:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const { addonBuilder, serveHTTP } = require('stremio-addon-sdk');
 const cron = require('node-cron');
 const { parsePlaylist } = require('./parser');
-const { updateCache } = require('./cache');
+const { updateCache, getCachedData } = require('./cache');
 const { catalogHandler, streamHandler } = require('./handlers');
 const config = require('./config');
 
 // Inizializza il manifest con i generi dalla playlist
 async function initializeAddon() {
     try {
-        // Estrai i generi dalla playlist
-        const { groups: extractedGroups } = await parsePlaylist(config.M3U_URL);
-        const groups = Array.from(extractedGroups);
-        console.log('Generi estratti inizialmente:', groups);
+        // Estrai i generi dalla playlist e aggiorna la cache
+        await updateCache();
+        const { genres } = getCachedData();
+        
+        console.log('Generi estratti inizialmente:', genres);
 
         // Crea le opzioni dei generi
-        const genreOptions = groups.map(genre => {
+        const genreOptions = genres.map(genre => {
             console.log('Creazione opzione per genere:', genre);
             return {
                 name: String(genre),
@@ -48,8 +49,8 @@ async function initializeAddon() {
         const builder = new addonBuilder(manifest);
 
         // Definisci gli handler
-        builder.defineCatalogHandler(args => catalogHandler(args, builder));
-        builder.defineStreamHandler(args => streamHandler(args, builder));
+        builder.defineCatalogHandler(args => catalogHandler(args));
+        builder.defineStreamHandler(args => streamHandler(args));
 
         return builder;
     } catch (error) {
@@ -62,15 +63,11 @@ async function initializeAddon() {
 async function startServer() {
     try {
         const builder = await initializeAddon();
-        
-        // Aggiorna la cache all'avvio
-        await updateCache(builder);
-        console.log('Cache aggiornata con successo all\'avvio.');
 
         // Aggiorna la cache ogni giorno alle 3:00 del mattino (solo se l'EPG è abilitato)
         if (config.enableEPG) {
             cron.schedule('0 3 * * *', () => {
-                updateCache(builder).catch(error => {
+                updateCache().catch(error => {
                     console.error('Errore nell\'aggiornamento della cache:', error);
                 });
             });
